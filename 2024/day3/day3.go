6376298a93ec11d917589ea5d0b90c67735e1544
Jonathan Fok kan <jonathan@fokkan.ca>
2024-12-03 14:42:23 -0500

    2024: day3 part 1

---
+++ 2024/day3/day3.go
@@ -0,0 +1,142 @@
+package day3
+
+import (
+	"strconv"
+)
+
+type itemType int
+
+const (
+	itemMulStart itemType = iota
+	itemMulEnd
+	itemComma
+	itemNumber
+	itemDo
+	itemDoNot
+)
+
+type Item struct {
+	typ itemType
+	val string
+}
+
+type Mul struct {
+	val1 int
+	val2 int
+}
+
+type Lexer struct {
+	input string
+	start int
+	pos   int
+	items []Item
+}
+
+type stateFn func(l *Lexer) stateFn
+
+func Run(input string) int {
+	program := Parse(input)
+	sum := ExecuteProgram(program)
+	return sum
+}
+
+func ExecuteProgram(program []Mul) int {
+	var sum int
+
+	for _, instr := range program {
+		sum += instr.val1 * instr.val2
+	}
+	return sum
+}
+
+func Parse(input string) []Mul {
+	items := Lex(input)
+	var program []Mul
+
+	for i := 0; i < len(items); {
+		if items[i].typ == itemMulStart && i+4 < len(items) && items[i+1].typ == itemNumber && items[i+2].typ == itemComma && items[i+3].typ == itemNumber && items[i+4].typ == itemMulEnd {
+			val1, _ := strconv.Atoi(items[i+1].val)
+			val2, _ := strconv.Atoi(items[i+3].val)
+			program = append(program, Mul{val1: val1, val2: val2})
+			i += 5
+		} else {
+			i += 1
+		}
+	}
+	return program
+}
+
+func Lex(input string) []Item {
+	lexer := &Lexer{
+		input: input,
+	}
+
+	for state := lexCorrupted(lexer); ; {
+		if lexer.pos >= len(input) {
+			break
+		}
+		state = state(lexer)
+	}
+	return lexer.items
+}
+
+func lexCorrupted(l *Lexer) stateFn {
+	if l.input[l.pos] == 'm' {
+		return lexMul
+	} else {
+		l.pos += 1
+		l.start = l.pos
+		return lexCorrupted
+	}
+}
+
+func lexMul(l *Lexer) stateFn {
+	if l.input[l.pos:l.pos+4] == "mul(" {
+		l.items = append(l.items, Item{typ: itemMulStart, val: string(l.input[l.pos : l.pos+4])})
+		l.pos += 4
+		l.start = l.pos
+		return lexNumber
+	} else {
+		l.pos += 1
+		l.start = l.pos
+		return lexCorrupted
+	}
+}
+
+func lexNumber(l *Lexer) stateFn {
+	if IsNumber(l.input[l.pos]) {
+		l.pos += 1
+		return lexNumber
+	} else {
+		if l.pos > l.start {
+			l.items = append(l.items, Item{typ: itemNumber, val: string(l.input[l.start:l.pos])})
+		}
+		l.start = l.pos
+		return lexCommaOrEndMul
+	}
+}
+
+func lexCommaOrEndMul(l *Lexer) stateFn {
+	if l.input[l.pos] == ',' {
+		l.items = append(l.items, Item{typ: itemComma, val: string(l.input[l.pos])})
+		l.pos += 1
+		l.start = l.pos
+		return lexNumber
+	} else if l.input[l.pos] == ')' {
+		l.items = append(l.items, Item{typ: itemMulEnd, val: string(l.input[l.pos])})
+		l.pos += 1
+		l.start = l.pos
+		return lexCorrupted
+	} else {
+		return lexCorrupted
+	}
+}
+
+func IsNumber(x byte) bool {
+	switch x {
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return true
+	default:
+		return false
+	}
+}
---
+++ 2024/day3/day3_test.go
@@ -0,0 +1,47 @@
+package day3
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+const smallInput = `xmul(2,4)%&`
+const exampleInput = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+
+func TestLex(t *testing.T) {
+	tokens := Lex(smallInput)
+
+	if len(tokens) != 5 {
+		t.Fatalf("unexpected result tokens = %#v", tokens)
+	}
+
+	fmt.Printf("tokens: %#v\n", tokens)
+}
+
+func TestParse(t *testing.T) {
+	program := Parse(exampleInput)
+
+	if len(program) != 4 {
+		t.Fatalf("Incorrect parsing got %#v", program)
+	}
+}
+
+func TestRun(t *testing.T) {
+	sum := Run(exampleInput)
+
+	if sum != 161 {
+		t.Fatalf("Expected = 161, got = %v", sum)
+	}
+}
+
+func TestRunPuzzleInput(t *testing.T) {
+	input, err := os.ReadFile("./input.txt")
+	if err != nil {
+		t.Fatalf("Could not read puzzle input: err = %s", err)
+	}
+
+	sum := Run(string(input))
+
+	fmt.Printf("# puzzle\nsum = %d\n", sum)
+}
